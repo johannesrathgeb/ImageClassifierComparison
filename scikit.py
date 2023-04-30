37039f6e3a70125a1ab497ac0fc5422b6dfc6672
Johannes Rathgeb <jrathgeb2001@gmail.com>
2023-04-30 23:42:39 +0200

    3 algorithms added

--- scikit.py
+++ scikit.py
@@ -8,12 +8,70 @@ from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
+from sklearn import tree
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+
 
 def unpickle(file):
     import pickle
     with open(file, 'rb') as fo:
         dict = pickle.load(fo, encoding='latin1')
     return dict
+
+def dt():
+    print('Decision Tree')
+    clf = tree.DecisionTreeClassifier()
+    clf.fit((data_batch_1['data'])[:500], (data_batch_1['labels'])[:500])
+    y_prediction = clf.predict((test_batch['data'])[:500])
+
+    score = accuracy_score(y_prediction, (test_batch['labels'])[:500])
+
+    print('{}% of samples were correctly classified'.format(str(score * 100)))
+    return
+
+def rf():
+    print('Random Forest')
+    clf = RandomForestClassifier()
+    clf.fit((data_batch_1['data'])[:500], (data_batch_1['labels'])[:500])
+    y_prediction = clf.predict((test_batch['data'])[:500])
+
+    score = accuracy_score(y_prediction, (test_batch['labels'])[:500])
+
+    print('{}% of samples were correctly classified'.format(str(score * 100)))
+    return
+
+def gbt():
+    print('Gradient Boosting Tree')
+    clf = GradientBoostingClassifier()
+    clf.fit((data_batch_1['data'])[:500], (data_batch_1['labels'])[:500])
+    y_prediction = clf.predict((test_batch['data'])[:500])
+
+    score = accuracy_score(y_prediction, (test_batch['labels'])[:500])
+
+    print('{}% of samples were correctly classified'.format(str(score * 100)))
+    return
+
+
+def svc():
+    # train classifier
+    classifier = SVC()
+
+    parameters = [{'gamma': [0.01, 0.001, 0.0001], 'C': [1, 10, 100, 1000]}]
+
+    grid_search = GridSearchCV(classifier, parameters)
+
+    grid_search.fit((data_batch_1['data'])[:500], (data_batch_1['labels'])[:500])
+
+    # test performance
+    best_estimator = grid_search.best_estimator_
+
+    y_prediction = best_estimator.predict((test_batch['data'])[:500])
+
+    score = accuracy_score(y_prediction, (test_batch['labels'])[:500])
+
+    print('{}% of samples were correctly classified'.format(str(score * 100)))
+    return
 
 # prepare data
 input_dir = '/home/johannes/Dokumente/GitHub/Bachelorarbeit/cifar-10-batches-py/'
@@ -45,28 +103,18 @@ labels = []
         data.append(img.flatten())
         labels.append(category_idx)"""
 
-data = np.asarray(data_batch_1['data'])
-labels = np.asarray(data_batch_1['labels'])
+data = np.asarray((data_batch_1['data'])[:500])
+labels = np.asarray((data_batch_1['labels'])[:500])
 
+print(len(data))
+#svc()
+dt()
+rf()
+gbt()
 # train / test split
-x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, shuffle=True, stratify=labels)
+#x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, shuffle=True, stratify=labels)
 
-# train classifier
-classifier = SVC()
 
-parameters = [{'gamma': [0.01, 0.001, 0.0001], 'C': [1, 10, 100, 1000]}]
 
-grid_search = GridSearchCV(classifier, parameters)
-
-grid_search.fit(x_train, y_train)
-
-# test performance
-best_estimator = grid_search.best_estimator_
-
-y_prediction = best_estimator.predict(x_test)
-
-score = accuracy_score(y_prediction, y_test)
-
-print('{}% of samples were correctly classified'.format(str(score * 100)))
 
 #pickle.dump(best_estimator, open('./model.p', 'wb'))
