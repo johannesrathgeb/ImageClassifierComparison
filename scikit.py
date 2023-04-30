96b200790189d39fce745cae207158ac15da47da
Johannes Rathgeb <jrathgeb2001@gmail.com>
2023-04-30 23:48:30 +0200

    Update scikit.py

--- scikit.py
+++ scikit.py
@@ -1,16 +1,12 @@
 import os
 import pickle
-
 import numpy as np
-from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 from sklearn import tree
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import GradientBoostingClassifier
 
-
 def unpickle(file):
-    import pickle
     with open(file, 'rb') as fo:
         dict = pickle.load(fo, encoding='latin1')
     return dict
@@ -51,7 +47,6 @@ def gbt():
 # prepare data
 input_dir = '/home/johannes/Dokumente/GitHub/Bachelorarbeit/cifar-10-batches-py/'
 
-
 data_batch_1=unpickle(input_dir + 'data_batch_1')
 data_batch_3=unpickle(input_dir + 'data_batch_3')
 data_batch_2=unpickle(input_dir + 'data_batch_2')
@@ -59,21 +54,12 @@ data_batch_4=unpickle(input_dir + 'data_batch_4')
 data_batch_5=unpickle(input_dir + 'data_batch_5')
 test_batch=unpickle(input_dir + 'test_batch')
 
-#print(len(data_batch_1['data']))
-#print(len(data_batch_2['data']))
-#print(len(data_batch_3['data']))
-#print(len(data_batch_4['data']))
-#print(len(data_batch_5['data']))
-#print(len(test_batch['data']))
-
-
 data = []
 labels = []
 data = np.asarray((data_batch_1['data'])[:500])
 labels = np.asarray((data_batch_1['labels'])[:500])
 
 print(len(data))
-#svc()
 dt()
 rf()
 gbt()
